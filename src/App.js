d4c44a7c9c5043e3188b6f7a0bb627d731281280
dks2001 <dheerendrak2001@gmail.com>
2023-10-01 19:34:21 +0530

    new code added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+}
+  from 'react-router-dom';
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
+import Page1 from './page1';
+import Home from './Home';
+import Page2 from './Page2';
 
 function App() {
+
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home/>}/>
+          <Route path="/page1" element={<Page1 />} />
+          <Route path="/page2" element={<Page2 />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
